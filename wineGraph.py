f0229f56786eba5685165e55dc2c35b2c5232d58
Sirawit <popeye-zz@hotmail.com>
2021-01-19 23:18:47 +0700

    finish HW1 & basic dot graphs

--- wineGraph.py
+++ wineGraph.py
@@ -4,10 +4,30 @@ import pandas as pd
 plt.style.use('bmh')
 df = pd.read_csv('testgraphredwine.csv')
 
-x = df['quality']
+# x (independent variable) = alcohol
+# y (dependent variable) = quality
+'''
+# All column
+x = df['alcohol'] 
+y = df['quality']
+
+# Dot Graph
+plt.xlabel('alcohol', fontsize = 18)
+plt.ylabel('quality', fontsize = 16)
+plt.scatter(x, y)
+'''
+
+# x (independent variable) = quality
+# y (dependent variable) = alcohol
+
+# All column
+x = df['quality'] 
 y = df['alcohol']
 
-# Line Graph
+# Dot Graph
 plt.xlabel('quality', fontsize = 18)
 plt.ylabel('alcohol', fontsize = 16)
-plt.bar(x, y)
+plt.scatter(x, y)
+
+# Show
+plt.show()
