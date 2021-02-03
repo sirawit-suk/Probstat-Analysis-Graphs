52a7448047e9b53aa52b49bcd71b93abc7045e8b
Sirawit <popeye-zz@hotmail.com>
2021-02-04 01:25:06 +0700

    add mean med mod standDeriv and Varance sample

--- wineGraph.py
+++ wineGraph.py
@@ -1,22 +1,26 @@
 import matplotlib.pyplot as plt 
 import pandas as pd
+import statistics as stc
 
 plt.style.use('bmh')
 df = pd.read_csv('testgraphredwine.csv')
 
 # x (independent variable) = alcohol
 # y (dependent variable) = quality
-'''
+
 # All column
 x = df['alcohol'] 
 y = df['quality']
 
+""" use this one
 # Dot Graph
 plt.xlabel('alcohol', fontsize = 18)
 plt.ylabel('quality', fontsize = 16)
 plt.scatter(x, y)
-'''
+"""
 
+
+'''
 # x (independent variable) = quality
 # y (dependent variable) = alcohol
 
@@ -28,6 +32,36 @@ y = df['alcohol']
 plt.xlabel('quality', fontsize = 18)
 plt.ylabel('alcohol', fontsize = 16)
 plt.scatter(x, y)
+'''
+alcoholMean = stc.mean(x)
+qualityMean = stc.mean(y)
+print("alcohol Mean :", alcoholMean)
+print("quality Mean :", qualityMean)
+
+alcoholMed = stc.median(x)
+qualityMed = stc.median(y)
+print("alcohol Med :", alcoholMed)
+print("quality Med :", qualityMed)
+
+alcoholMode = stc.mode(x)
+qualityMode = stc.mode(y)
+print("alcohol Mode :", alcoholMode)
+print("quality Mode :", qualityMode)
+
+alcoholSampleStandardDeviation = stc.stdev(x)
+qualitySampleStandardDeviation = stc.stdev(y)
+print("alcohol Sample Standard Deviation :", alcoholSampleStandardDeviation)
+print("quality Sample Standard Deviation :", qualitySampleStandardDeviation)
+
+alcoholSampleVariance = stc.variance(x)
+qualitySampleVariance = stc.variance(y)
+print("alcohol Sample Variance :", alcoholSampleVariance)
+print("quality Sample Variance :", qualitySampleVariance)
+
+# don't forget !!! the UNIT !!!!!
+
+plt.boxplot(x);
+plt.boxplot(y);
 
 # Show
 plt.show()
