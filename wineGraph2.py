e3d814b3910e6f0dbb463a538377aee580ec2feb
Sirawit <popeye-zz@hotmail.com>
2021-02-21 15:45:37 +0700

    winegraph2 edited

--- wineGraph2.py
+++ wineGraph2.py
@@ -38,7 +38,7 @@ al_func[0].axis(ymax=1)
 
 al_func[1].set_title('Alcohol in Red Wine (Cumulative Distribution Function (CDF))')
 al_func[1].set_xlabel("Alcohol (%/volume)")
-al_func[1].set_ylabel("Cumulative Probability")
+al_func[1].set_ylabel("Probability")
 al_func[1].plot(al_bins_count[1:], al_cdf, color="red", label="CDF") 
 al_func[1].legend()
 
@@ -55,7 +55,7 @@ qu_func[0].axis(ymax=1)
 
 qu_func[1].set_title('Quality of Red Wine (Cumulative Distribution Function (CDF))')
 qu_func[1].set_xlabel("Quality (lv.1-10)")
-qu_func[1].set_ylabel("Cumulative Probability")
+qu_func[1].set_ylabel("Probability")
 qu_func[1].plot(qu_bins_count[1:], qu_cdf, color="red", label="CDF") 
 qu_func[1].legend()
 
@@ -63,20 +63,19 @@ qu_func[1].legend()
 figure, total = plt.subplots(1, 2, figsize=(14, 5))
 plt.tight_layout(pad=4, w_pad=3, h_pad=1.0)
 
-total[0].set_title('Alcohol in Red Wine (PDF and CDF)')
+total[0].set_title('Alcohol in Red Wine Summary Graph (PDF and CDF)')
 total[0].set_xlabel("Alcohol (%/volume)")
-qu_func[1].set_ylabel("Probability")
+total[0].set_ylabel("Probability")
 total[0].plot(al_bins_count[1:], al_pdf, color="green", label="PDF") 
 total[0].plot(al_bins_count[1:], al_cdf, color="red", label="CDF") 
 total[0].legend()
 
 total[1].set_title('Quality of Red Wine Summary Graph (PMF and CDF)')
 total[1].set_xlabel("Quality (lv.1-10)")
-qu_func[1].set_ylabel("Probability")
+total[1].set_ylabel("Probability")
 total[1].plot(qu_bins_count[1:], qu_pdf, color="green", label="PDF") 
 total[1].plot(qu_bins_count[1:], qu_cdf, color="red", label="CDF") 
 total[1].legend()
-
 
 
   
